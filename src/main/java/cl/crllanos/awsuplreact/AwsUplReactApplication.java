ffac8a0d5117cca5e696323dee6ba2fba958919d
Cristian <Cristian Llanos@gmail.com>
2023-09-14 04:44:23 -0300

    first commit, Tomcat started

---
+++ src/main/java/cl/crllanos/awsuplreact/AwsUplReactApplication.java
@@ -0,0 +1,13 @@
+package cl.crllanos.awsuplreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsUplReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsUplReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/crllanos/awsuplreact/AwsUplReactApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.crllanos.awsuplreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsUplReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
