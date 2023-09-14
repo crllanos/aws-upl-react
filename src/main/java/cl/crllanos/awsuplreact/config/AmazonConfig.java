924c9a9db56775fcb7c01d801decfe7e64995512
Cristian <Cristian Llanos@gmail.com>
2023-09-14 05:24:20 -0300

    amazon s3 client

---
+++ src/main/java/cl/crllanos/awsuplreact/config/AmazonConfig.java
@@ -0,0 +1,32 @@
+package cl.crllanos.awsuplreact.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Value("app.config.amazon.key")
+    private String key;
+
+    @Value("app.config.amazon.secret")
+    private String secret;
+
+    @Bean
+    public AmazonS3 s3(){
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(key, secret)
+                        ))
+                .build();
+    }
+
+
+}
